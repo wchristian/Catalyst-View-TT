d97e99999f9810804910cee3b3a61866bd3080fa
Sebastian Riedel <sri@labs.kraih.com>
2005-04-13 15:24:49 +0000

    some updates

--- lib/Catalyst/Helper/View/TT.pm
+++ lib/Catalyst/Helper/View/TT.pm
@@ -22,17 +22,39 @@ Helper for TT Views.
 
 sub mk_compclass {
     my ( $self, $helper ) = @_;
-    my $file  = $helper->{file};
-    my $class = $helper->{class};
-    $helper->mk_file( $file, <<"EOF");
-package $class;
+    my $file = $helper->{file};
+    $helper->mk_file( 'compclass', $file );
+}
+
+=head1 SEE ALSO
+
+L<Catalyst::Manual>, L<Catalyst::Test>, L<Catalyst::Request>,
+L<Catalyst::Response>, L<Catalyst::Helper>
+
+=head1 AUTHOR
+
+Sebastian Riedel, C<sri@oook.de>
+
+=head1 LICENSE
+
+This library is free software . You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
+
+__DATA__
+
+__compclass__
+package [% class %];
 
 use strict;
 use base 'Catalyst::View::TT';
 
 =head1 NAME
 
-$class - TT View Component
+[% class %] - TT View Component
 
 =head1 SYNOPSIS
 
@@ -45,26 +67,6 @@ Very nice component.
 =head1 AUTHOR
 
 Clever guy
-
-=head1 LICENSE
-
-This library is free software . You can redistribute it and/or modify it under
-the same terms as perl itself.
-
-=cut
-
-1;
-EOF
-}
-
-=head1 SEE ALSO
-
-L<Catalyst::Manual>, L<Catalyst::Test>, L<Catalyst::Request>,
-L<Catalyst::Response>, L<Catalyst::Helper>
-
-=head1 AUTHOR
-
-Sebastian Riedel, C<sri@oook.de>
 
 =head1 LICENSE
 
